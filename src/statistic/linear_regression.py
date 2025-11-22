9bf4f495525b7e09f0cc0776f0eb3b49aa08e0ca
Wendell Liu <cuk.bas@gmail.com>
2025-11-22 14:48:48 +0800

    feat: simultaneous_linear_regression

--- src/statistic/linear_regression.py
+++ src/statistic/linear_regression.py
@@ -13,3 +13,17 @@ def simple_linear_regression_report(y_series, x_series):
     model = simple_linear_regression_model(y_series, x_series)
 
     return model.summary()
+
+
+def simultaneous_linear_regression_model(y_series, x_series):
+    X = sm.add_constant(x_series)  # Adds intercept
+    Y = y_series
+    model = sm.OLS(Y, X).fit()
+
+    return model
+
+
+def simultaneous_linear_regression_report(y_series, x_series):
+    model = simultaneous_linear_regression_model(y_series, x_series)
+
+    return model.summary()
